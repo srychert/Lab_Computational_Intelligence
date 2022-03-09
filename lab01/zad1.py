725fd613ade785e7da8608768e6956b9495ff988
SebastianRychert <sebastian_rychert@wp.pl>
2022-03-09 08:54:55 +0100

    zad1 select_primes

--- lab01/zad1.py
+++ lab01/zad1.py
@@ -11,3 +11,14 @@ def prime(n):
 print(prime(3))
 print(prime(4))
 print(prime(49))
+
+
+def select_primes(x):
+    result = []
+    for e in x:
+        if prime(e):
+            result.append(e)
+    return result
+
+
+print(select_primes([3, 6, 11, 25, 19]))
