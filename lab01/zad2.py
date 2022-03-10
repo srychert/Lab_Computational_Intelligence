c8d0f18d3bca51600e5e1ad54c5e96394e5a68e6
Sebastian Rychert <sebastian_rychert@wp.pl>
2022-03-10 10:42:10 +0100

    zad2 f-h plus zmiany

--- lab01/zad2.py
+++ lab01/zad2.py
@@ -1,6 +1,14 @@
 import math
 import random
 
+
+def my_print(obj, depth):
+    if len(obj) > depth:
+        print(f"{str(obj[:depth])[:-1]}, ... , {obj[-1]}]")
+    else:
+        print(obj)
+
+
 vector1 = [3, 8, 9, 10, 12]
 vector2 = [8, 7, 7, 5, 6]
 
@@ -13,7 +21,7 @@ def sum_vectors(v1, v2):
     return v_sum
 
 
-print(sum_vectors(vector1, vector2))
+print(f"suma wektorów: {sum_vectors(vector1, vector2)}")
 
 
 def multiply_vectors(v1, v2):
@@ -23,7 +31,7 @@ def multiply_vectors(v1, v2):
     return v_mult
 
 
-print(multiply_vectors(vector1, vector2))
+print(f"iloczyn wektorów: {multiply_vectors(vector1, vector2)}")
 
 
 # b)
@@ -34,7 +42,7 @@ def iloczyn_skalarny(v1, v2):
     return result
 
 
-print(iloczyn_skalarny(vector1, vector2))
+print(f"iloczyn skalarny: {iloczyn_skalarny(vector1, vector2)}")
 
 
 # c)
@@ -45,19 +53,22 @@ def vector_length(v):
     return math.sqrt(sum_squared)
 
 
-print(vector_length(vector1))
-print(vector_length(vector2))
+print(
+    f"długości euklidesowe: {vector_length(vector1), vector_length(vector2)}")
 
 
 # d)
 vector_random = []
 for ele in range(50):
     vector_random.append(random.randint(1, 100))
-print(vector_random)
+
+print("=================================")
+print("Losowy wektor z zakresu [1, 100]:")
+my_print(vector_random, 10)
 
 
 # e)
-def v_mean(v):
+def vector_mean(v):
     result = 0
     for i in v:
         result += i
@@ -65,17 +76,17 @@ def v_mean(v):
 
 
 def standard_deviation(v):
-    mean = v_mean(v)
+    mean = vector_mean(v)
     variance_sum = 0
     for i in v:
         variance_sum += (i - mean)**2
     return math.sqrt(variance_sum/len(v))
 
 
-print(v_mean(vector_random))
-print(min(vector_random))
-print(max(vector_random))
-print(standard_deviation(vector_random))
+print(f"średnia: {vector_mean(vector_random)}")
+print(f"minimum: {min(vector_random)}")
+print(f"maximum: {max(vector_random)}")
+print(f"odchylenie standardowe: {standard_deviation(vector_random)}")
 
 
 # f)
@@ -83,12 +94,55 @@ def vector_normalize(v):
     new_vector = []
     v_min = min(v)
     v_max = max(v)
-    for i in range(len(v)):
-        new_ele = (v[i] - v_min) / (v_max - v_min)
+    for i in v:
+        new_ele = (i - v_min) / (v_max - v_min)
         new_vector.append(new_ele)
     return new_vector
 
 
-print(vector_normalize(vector_random))
+normalized_vector = vector_normalize(vector_random)
+print("=================================")
+print("Wektor znormalizowany:")
+my_print(normalized_vector, 10)
+print(
+    f"dawny max: {max(vector_random)} na pozycji {vector_random.index(max(vector_random))}"
+    f"\nnowa wartość na tej pozycji: {normalized_vector[vector_random.index(max(vector_random))]}")
 
-# g) Dokończ
+
+# g)
+def vector_standardize(v):
+    new_vector = []
+    mean = vector_mean(v)
+    devi = standard_deviation(v)
+    for i in v:
+        new_ele = (i - mean) / devi
+        new_vector.append(new_ele)
+    return new_vector
+
+
+print("=================================")
+print("Wektor ustandaryzowany:")
+standardized_vector = vector_standardize(vector_random)
+my_print(standardized_vector, 10)
+print(f"Nowa średnia: {vector_mean(standardized_vector)}")
+print(f"Nowe odchylenie std: {standard_deviation(standardized_vector)}")
+
+
+# h)
+def vector_discretization(v, start=0, stop=100, step=10):
+    new_vector = []
+    for e in v:
+        for i in range(start, stop, step):
+            right_end = i + step-1
+            symbol = f"{right_end+1})"
+            if(i == stop-step):
+                right_end = i + step
+                symbol = f"{right_end}]"
+            if right_end > e >= i:
+                new_vector.append(f"[{i}, {symbol}")
+    return new_vector
+
+
+print("=================================")
+print("Wektor po dyskretyzacji:")
+my_print(vector_discretization(vector_random), 10)
