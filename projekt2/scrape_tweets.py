8f20343350bbb157c538ce86ef4a7c2df2c8c98d
Sebastian Rychert <sebastian_rychert@wp.pl>
2022-06-08 00:32:54 +0200

    scraping tweets

--- projekt2/scrape_tweets.py
+++ projekt2/scrape_tweets.py
@@ -1,38 +1,16 @@
-from subprocess import call
-import snscrape.modules.twitter
 import json
-from util import filter_words
-from util import lemmatize_words
-from nltk.tokenize import word_tokenize
-import nltk
-from nltk.corpus import stopwords
 
-nltk.download('stopwords')
-nltk.download('wordnet')
-
-# To get the latest 100 tweets with the hashtag #MonkeypoxVirus:
-# call(["snscrape", "--max-results", "100", "--jsonl", "twitter-hashtag", "MonkeypoxVirus"])
+import snscrape.modules.twitter
 
 tweets = []
 
-# scraper = snscrape.modules.twitter.TwitterSearchScraper('#MonkeypoxVirus since:2022-05-01 until:2022-05-29 near:"Gdańsk" within:"60km"')
-scraper = snscrape.modules.twitter.TwitterSearchScraper('#virus since:2020-06-01')
+scraper = snscrape.modules.twitter.TwitterSearchScraper('#ukraine #war” since:2022-02-20')
 for i, tweet in enumerate(scraper.get_items()):
-    if i > 10:
+    if i > 30000:
+        print(tweet.date)
         break
     if (tweet.lang == "en"):
-        tweets.append(tweet.content)
-
-print(tweets[0])
+        tweets.append({"content": tweet.content, "date": tweet.date})
 
 with open("tweets.json", "w") as f:
-    f.write(json.dumps(tweets, indent=4))
-
-stop_words = set(stopwords.words("english"))
-
-for content in tweets:
-    tokenized = word_tokenize(content)
-    filtered = filter_words(tokenized, stop_words)
-    lem = lemmatize_words(filtered)
-
-    print(lem)
+    f.write(json.dumps(tweets, indent=4, default=str))
